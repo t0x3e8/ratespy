029e1a6e8908b98c59151cc400466a24545a2b65
Jarek <t0x3e8@gmail.com>
2024-04-07 20:58:31 +0200

    Database storing optimization for redundancy

--- rate_spy/myapp/utils.py
+++ rate_spy/myapp/utils.py
@@ -2,13 +2,17 @@ from django.utils import timezone
 from .models import LinkSetting
 
 def save_to_database(name, url, interval_in_minutes):
-    link_setting = LinkSetting(
-        name=name,
-        url=url,
-        created_at=timezone.now(),
-        interval_in_minutes=interval_in_minutes
-    )
-    link_setting.save()
+    if not LinkSetting.objects.filter(name=name, url=url, interval_in_minutes=interval_in_minutes).exists():
+        link_setting = LinkSetting(
+            name=name,
+            url=url,
+            created_at=timezone.now(),
+            interval_in_minutes=interval_in_minutes
+        )
+        link_setting.save()
+        return True
+    else:
+        return False
 
 def process_form_data(cleaned_data):
     # Insert your method to process the form data here
--- rate_spy/myapp/views.py
+++ rate_spy/myapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .forms import RatingForm
 from .utils import save_to_database, process_form_data
+
 
 # Create your views here.
 def index_view(request):
@@ -14,10 +16,14 @@ def rating_view(request):
     if request.method == 'POST':
         form = RatingForm(request.POST)
         if form.is_valid():
-            save_to_database(
+            save_to_database_result = save_to_database(
                 form.cleaned_data['name'],
                 form.cleaned_data['url'],
-                form.cleaned_data['interval_in_minutes'])
+                form.cleaned_data['interval_in_minutes']
+                )
+            
+            if not save_to_database_result:
+                messages.warning(request, 'Data was not saved because it already exists.')
             
             process_form_data(form.cleaned_data)
             form = RatingForm()  
