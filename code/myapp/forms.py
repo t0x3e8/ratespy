7a75c5d41be5a483b6cb3d2ac03c8579b195acf1
Jarek <t0x3e8@gmail.com>
2024-04-15 18:50:35 +0200

    Remove files that are now ignored

--- code/myapp/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class RatingForm(forms.Form):
-    url = forms.URLField(label='URL of the App')
-    name = forms.CharField(max_length=100, required=False)
-    interval_in_minutes = forms.IntegerField(label='Interval (minutes)', min_value=1, help_text='Specify the time interval in minutes.')
